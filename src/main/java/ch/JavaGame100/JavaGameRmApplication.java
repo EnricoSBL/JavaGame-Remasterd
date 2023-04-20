47982d34a0f0644c3047e9af12f08b8e4a807436
EnricoSBL <enrico.proietto@bbzbl-it.ch>
2023-04-20 19:09:18 +0200

    inicial commit

---
+++ src/main/java/ch/JavaGame100/JavaGameRmApplication.java
@@ -0,0 +1,13 @@
+package ch.JavaGame100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGameRmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGameRmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/JavaGame100/JavaGameRmApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.JavaGame100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGameRmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
